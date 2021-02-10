83a95b9fb741993d402ad0107576a30c204a6f36
Norbi <norbimacsali445@gmail.com>
2021-02-10 10:56:27 +0100

    6

--- fibonacciRecursive/src/Main.java
+++ fibonacciRecursive/src/Main.java
@@ -1,26 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Main.finbonacciIterative(5));
+        System.out.println(Main.fibonacciMemoized(10));
     }
 
     public static int fibonacciRecursive(int num) { //2^n - time
-        if(num < 2) {
+        if (num < 2) {
             return num;
         }
 
         return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
     }
 
-    public static int finbonacciIterative(int num) { //O(n) - time
+    public static Map<Integer, Integer> cache = new HashMap<>();
+
+    public static int fibonacciMemoized(int n) {
+        if (cache.containsKey(n)) {
+            return cache.get(n);
+        } else if (n < 2) {
+            return n;
+        } else {
+            cache.put(n, fibonacciMemoized(n - 1) + fibonacciMemoized(n - 2));
+            return cache.get(n);
+        }
+
+    }
+
+
+    public static int fibonacciIterative(int num) { //O(n) - time
         List<Integer> list = new ArrayList<>();
         list.add(0);
         list.add(1);
 
-        for(int i = 2; i <= num; i++) {
-            list.add(list.get(i-1) + list.get(i-2));
+        for (int i = 2; i <= num; i++) {
+            list.add(list.get(i - 1) + list.get(i - 2));
         }
 
         return list.get(num);
