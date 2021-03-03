46c5e87418de263f7e60dd5ed46ebe706df2ca48
Norbi <norbimacsali445@gmail.com>
2021-03-03 17:53:21 +0100

    23

--- SymmetricTree/src/com/company/Main.java
+++ SymmetricTree/src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -26,6 +29,41 @@ public class Main {
             return false;
         }
         return isSymmetric(left.left,right.right) && isSymmetric(left.right,right.left);
+    }
+
+    public boolean isSymmetricIterative(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+
+            TreeNode n1 = queue.poll();
+            TreeNode n2 = queue.poll();
+
+            if(n1 == null && n2 == null) {
+                continue;
+            }
+            if(n1 == null || n2 == null) {
+                return false;
+            }
+            if(n1.val != n2.val) {
+                return false;
+            }
+
+            queue.add(n1.left);
+            queue.add(n2.right);
+            queue.add(n1.right);
+            queue.add(n2.left);
+
+        }
+
+        return true;
     }
 }
 
