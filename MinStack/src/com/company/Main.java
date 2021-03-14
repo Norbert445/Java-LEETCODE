196222ecbe006ceda1b5060c973cc7dd4e7ae5e8
Norbi <norbimacsali445@gmail.com>
2021-03-14 12:54:09 +0100

    23

---
+++ MinStack/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+
+        if(minStack.isEmpty()) {
+            minStack.push(x);
+        } else {
+            if(x <= minStack.peek()) {
+                minStack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        if(stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
