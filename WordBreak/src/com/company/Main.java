9ea3dacde26aaef5c72756eec773f5b05b4c21de
Norbi <norbimacsali445@gmail.com>
2021-04-17 21:15:36 +0200

    36

--- WordBreak/src/com/company/Main.java
+++ WordBreak/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -12,9 +14,9 @@ public class Main {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
 
-        for(int len = 1; len <= s.length(); len++) {
-            for(int i = 0; i < len; i++) {
-                if(dp[i] && wordDict.contains(s.substring(i,len))) {
+        for (int len = 1; len <= s.length(); len++) {
+            for (int i = 0; i < len; i++) {
+                if (dp[i] && wordDict.contains(s.substring(i, len))) {
                     dp[len] = true;
                     break;
                 }
@@ -22,4 +24,24 @@ public class Main {
         }
         return dp[s.length()];
     }
+
+    public boolean wordBreakRecursive(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for (String str : wordDict) {
+            set.add(str);
+        }
+        return dfs(s, set);
+    }
+
+    public boolean dfs(String s, Set<String> set) {
+        if (s.equals("")) return true;
+
+        for(int i = 1; i  <= s.length(); i++) {
+            if(set.contains(s.substring(0,i)) && dfs(s.substring(i,s.length()),set)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
