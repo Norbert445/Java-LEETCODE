deb26ca171f7d085a28ec00fd009a4d65a9bb242
Norbi <norbimacsali445@gmail.com>
2021-04-26 10:29:22 +0200

    39

--- CombinationSum/src/com/company/Main.java
+++ CombinationSum/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(candidates,0,target,new ArrayList<>(),list);
+        return list;
+    }
+
+    public void backtrack(int[] candidates, int start, int target, List<Integer> temp, List<List<Integer>> result) {
+        if(target < 0) {
+            return;
+        }
+        if(target == 0) {
+            result.add(new ArrayList<>(temp));
+        }
+        for(int i = start; i < candidates.length; i++) {
+            temp.add(candidates[i]);
+            backtrack(candidates,i,target - candidates[i],temp,result);
+            temp.remove(temp.size() - 1);
+        }
     }
 }
