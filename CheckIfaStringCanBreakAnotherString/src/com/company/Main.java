e3f98602025e8be3c3600dcc86749d6e4922281e
Norbi <norbimacsali445@gmail.com>
2021-05-15 12:16:43 +0200

    51

--- CheckIfaStringCanBreakAnotherString/src/com/company/Main.java
+++ CheckIfaStringCanBreakAnotherString/src/com/company/Main.java
@@ -32,4 +32,30 @@ public class Main {
         if(valid) return true;
         else return false;
     }
+
+    public boolean checkIfCanBreakOptimized(String s1, String s2) {
+        char[] str_arr1 = s1.toCharArray();
+        char[] str_arr2 = s2.toCharArray();
+
+        Arrays.sort(str_arr1);
+        Arrays.sort(str_arr2);
+
+        int pos_or_neg = 0;
+
+        for(int i = 0; i < str_arr1.length; i++) {
+            int difference = str_arr1[i] - str_arr2[i];
+            if(pos_or_neg == 0)  {
+                if(difference > 0) {
+                    pos_or_neg = 1;
+                } else if(difference < 0) {
+                    pos_or_neg = -1;
+                }
+            }
+
+            if(difference > 0 && pos_or_neg < 0) return false;
+            if(difference < 0 && pos_or_neg > 0) return false;
+        }
+
+        return true;
+    }
 }
