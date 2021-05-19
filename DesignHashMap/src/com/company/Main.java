59fa4ced06c4231332c5ff99e174723d52a6180e
Norbi <norbimacsali445@gmail.com>
2021-05-19 12:46:53 +0200

    52

---
+++ DesignHashMap/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class MyHashMap {
+    int[] arr;
+
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        arr = new int[1000001];
+        Arrays.fill(arr,-1);
+    }
+
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        arr[key] = value;
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+       return arr[key];
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        arr[key] = -1;
+    }
+}
