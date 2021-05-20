5bc88c99556dde0ce842d5e0f3e60052a2535daf
Norbi <norbimacsali445@gmail.com>
2021-05-20 09:29:03 +0200

    54

--- DesignHashMap/src/com/company/Main.java
+++ DesignHashMap/src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -10,27 +9,88 @@ public class Main {
     }
 }
 
-class MyHashMap {
-    int[] arr;
+class MyHashMapLinkedList {
+    LinkedList<Entry>[] map;
+    int size = 769;
 
     /** Initialize your data structure here. */
+    public MyHashMapLinkedList() {
+        map = new LinkedList[size];
+    }
+
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        int hash = key % size;
+        if(map[hash] == null) {
+            map[hash] = new LinkedList<>();
+            map[hash].add(new Entry(key,value));
+        } else {
+            for(Entry entry : map[hash]) {
+                if(entry.key == key) {
+                    entry.val = value;
+                    return;
+                }
+            }
+            map[hash].add(new Entry(key,value));
+        }
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        int hash = key % size;
+        if(map[hash] == null) {
+            return -1;
+        } else {
+            for(Entry entry : map[hash]) {
+                if(entry.key == key) {
+                    return entry.val;
+                }
+            }
+            return -1;
+        }
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        int hash = key % size;
+        if(map[hash] == null) return;
+        for(Entry entry : map[hash]) {
+            if(entry.key == key) {
+                map[hash].remove(entry);
+                return;
+            }
+        }
+
+    }
+}
+
+class Entry {
+    int key;
+    int val;
+
+    public Entry(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
+
+/*class MyHashMap {
+    int[] arr;
+
     public MyHashMap() {
         arr = new int[1000001];
         Arrays.fill(arr,-1);
     }
 
-    /** value will always be non-negative. */
     public void put(int key, int value) {
         arr[key] = value;
     }
 
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
        return arr[key];
     }
 
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
         arr[key] = -1;
     }
-}
+}*/
