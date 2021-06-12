2ff36392661ac3e45b7ffb8abcbeb06d80679713
Norbi <norbimacsali445@gmail.com>
2021-06-12 14:42:19 +0200

    70

--- SignOfTheProductOfAnArray/src/com/company/Main.java
+++ SignOfTheProductOfAnArray/src/com/company/Main.java
@@ -7,7 +7,14 @@ public class Main {
     }
 
     public int arraySign(int[] nums) {
-        //Nedokoncene
-        return 0;
+        int negative = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0) return 0;
+            if(nums[i] < 0) negative++;
+        }
+
+        if(negative % 2 == 0) return 1;
+        else return -1;
     }
 }
