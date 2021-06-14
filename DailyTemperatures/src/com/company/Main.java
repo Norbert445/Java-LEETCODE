2e3c5a384ea6871872ab423bfabb0b824d99f36a
Norbi <norbimacsali445@gmail.com>
2021-06-14 09:35:41 +0200

    72

--- DailyTemperatures/src/com/company/Main.java
+++ DailyTemperatures/src/com/company/Main.java
@@ -1,20 +1,21 @@
 package com.company;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
     public int[] dailyTemperatures(int[] temperatures) {
         int[] res = new int[temperatures.length];
 
-        for(int i = 0; i < temperatures.length; i++) {
+        for (int i = 0; i < temperatures.length; i++) {
             res[i] = 0;
-            for(int j = i + 1; j < temperatures.length; j++) {
-                if(temperatures[j] > temperatures[i]) {
+            for (int j = i + 1; j < temperatures.length; j++) {
+                if (temperatures[j] > temperatures[i]) {
                     res[i] = j - i;
                     break;
                 }
@@ -24,23 +25,19 @@ public class Main {
         return res;
     }
 
-    //Nedokoncene
-    /*public int[] dailyTemperaturesOptimized(int[] temperatures) {
+    public int[] dailyTemperaturesOptimized(int[] temperatures) {
         int[] res = new int[temperatures.length];
         Stack<Integer> stack = new Stack<>();
 
-        for(int i = temperatures.length - 1; i >= 0; i--) {
-
-            if(!stack.isEmpty()) {
-                while(!stack.isEmpty() && temperatures[i] > stack.peek()) {
-                    stack.pop();
-                }
-                stack.push(temperatures[i]);
-            } else {
-                stack.push(temperatures[i]);
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
             }
+            if (!stack.isEmpty())
+                res[i] = stack.peek() - i;
+            stack.push(i);
         }
 
-        return new int[]{};
-    }*/
+        return res;
+    }
 }
---
+++ TransponseMatrix/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public int[][] transpose(int[][] matrix) {
+        //Nedokoncene
+        int[][] res = new int[matrix[0].length][matrix.length];
+
+        return null;
+    }
+}
