d665a763b506b8b1e3952829689d148b1cd34e76
Norbi <norbimacsali445@gmail.com>
2021-06-17 09:37:47 +0200

    75

--- SetMismatch/src/com/company/Main.java
+++ SetMismatch/src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +14,23 @@ public class Main {
     //Nedokoncene
     public int[] findErrorNums(int[] nums) {
         int[] res = new int[2];
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums) {
+            if(set.contains(num)) {
+                res[0] = num;
+            } else {
+                set.add(num);
+            }
+        }
+
+        for(int i = 1; i <= nums.length; i++) {
+            if(!set.contains(i)) {
+                res[1] = i;
+                break;
+            }
+        }
+
+        return res;
     }
 }
